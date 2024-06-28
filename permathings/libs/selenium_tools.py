135ea34e486c20da62d34c30f46cd95ecadbf49e
Paul Calhoun <paul.c.calhoun@gmail.com>
2024-06-28 16:26:40 -0400

    Update selenium_tools.py

--- permathings/libs/selenium_tools.py
+++ permathings/libs/selenium_tools.py
@@ -14,3 +14,31 @@ parent_dir = os.path.dirname(this_modules_path)
 sys.path.append(parent_dir)
 
 from docker_tools import run_docker_container
+
+def init_selenium_docker(driver_port=4444, vnc_port=7900):
+    container = run_docker_container("selenium/standalone-firefox", {f"{driver_port}/tcp": driver_port, f"{vnc_port}/tcp": vnc_port}, {}, {}, detach=True)
+    return container
+
+def stop_and_remove_selenium_docker(container):
+    container.stop()
+    container.remove()
+
+def init_selenium_driver_inside_docker(driver_port=4444):
+    options = Options()
+    driver = webdriver.Remote(command_executor=f"http://localhost:{driver_port}/wd/hub", options=options)
+    return driver
+
+def test_tools():
+    container = init_selenium_docker()
+    time.sleep(3)
+    driver = init_selenium_driver_inside_docker()
+    driver.get("https://yahoo.com")
+    time.sleep(5)
+    #print page text
+    source = driver.page_source
+    #soup = BeautifulSoup(source, "html.parser")
+    #text = soup.get_text()
+    text = html2text.html2text(source)
+    print(text)
+    stop_and_remove_selenium_docker(container)
+    print("Test done")
