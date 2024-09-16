3fa9cd46615f90839c57f8ab0c3eed67a0aa971c
Paul Calhoun <paul.c.calhoun@gmail.com>
2024-09-16 19:15:36 -0400

    Update 1_install_docker_engine.sh

--- permathings/prereqs/1_install_docker_engine.sh
+++ permathings/prereqs/1_install_docker_engine.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg -y; done
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
